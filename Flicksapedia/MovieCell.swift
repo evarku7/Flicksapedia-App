25b1999d10b7e8f25f5ffabcdbd2885add2457fc
alexp805@ufl.edu <AlexanderPayne@Alexanders-MacBook-Pro.local>
2016-01-10 22:37:31 -0500

    added poster image and cocoa pods

--- Flicksapedia/MovieCell.swift
+++ Flicksapedia/MovieCell.swift
@@ -14,6 +14,7 @@ class MovieCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
     
+    @IBOutlet weak var posterView: UIImageView!
     
     
     override func awakeFromNib() {
--- Flicksapedia/MoviesViewController.swift
+++ Flicksapedia/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AFNetworking
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -37,7 +38,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                         data, options:[]) as? NSDictionary {
                             NSLog("response: \(responseDictionary)")
                             
-                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                            self.movies = responseDictionary["results"] as? [NSDictionary]
                             self.tableView.reloadData()
                     }
                 }
@@ -67,10 +68,15 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
+        let posterPath = movie["poster_path"] as! String
+        let baseURL = "http://image.tmdb.org/t/p/w500"
+        
+        let imageURL = NSURL(string: baseURL + posterPath)
+        
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-        
+        cell.posterView.setImageWithURL(imageURL!)
         
         print("row \(indexPath.row)")
         return cell
